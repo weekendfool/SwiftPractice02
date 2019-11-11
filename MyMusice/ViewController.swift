35dbceb28f1cd60101368494c71c5ef6ca60ab66
Ohara <nori.tukareta@gmail.com>
2019-11-11 18:51:44 +0900

    Initial Commit

---
+++ MyMusice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusice
+//
+//  Created by 尾原徳泰 on 2019/11/11.
+//  Copyright © 2019 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
