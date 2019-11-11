2af13c08bd8468831daba374fcf9d11b3ae611f4
Ohara <nori.tukareta@gmail.com>
2019-11-11 21:27:33 +0900

    test3

--- MyMusice/ViewController.swift
+++ MyMusice/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    var cymbalPlayer = AVAudioPlayer()
 
-//hello test2
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            cymbalPlayer.play()
+        } catch {
+            print("シンバルで、エラーが発生しました！")
+        }
+    }
+    
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            guitarPlayer.play()
+        } catch {
+            print("ギターで、エラーが発生しました！")
+        }
+    }
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    var backmusicPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+            
+            backmusicPlayer.mumberOfLoops = -1
+            backmusicPlayer.play()
+        } catch {
+            print("エラーが発生しました!")
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    
+    //hello test2
 }
 
