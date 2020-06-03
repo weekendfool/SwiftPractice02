1aa8e64022a392aec91b36fa3074a771fc398662
Ohara <nori.tukareta@gmail.com>
2020-06-03 20:54:28 +0900

    プレイグラウンドの実験

---
+++ MyPlayground3.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+
+var a = 10
+var b = 20
+var c = a + b
+
+let str1 = "123"
+let number1 = (str1 as NSString).integerValue
+
+
+
+let str2 = "3.1415"
+let number2 = (str2 as NSString).floatValue
