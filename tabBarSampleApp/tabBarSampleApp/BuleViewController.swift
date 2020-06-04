27e600426eed12891b7bda356a20c79c5187f789
Ohara <nori.tukareta@gmail.com>
2020-06-04 21:23:58 +0900

    tabBarSampleApp作成

---
+++ tabBarSampleApp/tabBarSampleApp/BuleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BuleViewController.swift
+//  tabBarSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/04.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class BuleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func pressButton(_ sender: Any) {
+        print("blue")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tabBarSampleApp/tabBarSampleApp/GreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GreenViewController.swift
+//  tabBarSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/04.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pressButton(_ sender: Any) {
+        print("green")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
