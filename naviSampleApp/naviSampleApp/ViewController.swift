3b3f661b29dae62a732373a94bd129c3a0e0f64b
Ohara <nori.tukareta@gmail.com>
2020-06-07 20:07:37 +0900

    navigation遷移サンプル

---
+++ naviSampleApp/naviSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  naviSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/07.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
