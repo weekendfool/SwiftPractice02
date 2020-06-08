2aaa3903bd677d0c4c3cd7b16e7e210739011d01
Ohara <nori.tukareta@gmail.com>
2020-06-09 01:49:47 +0900

    indexpathについて

--- tableViewSampleApp/tableViewSampleApp/ViewController.swift
+++ tableViewSampleApp/tableViewSampleApp/ViewController.swift
@@ -15,14 +15,24 @@ class ViewController: UIViewController, UITableViewDataSource {
         // Do any additional setup after loading the view.
     }
 
-    //何回呼び出すのか
+    //何回呼び出すのか、行数を返す
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
-    //呼び出される。何を表示するのか
+    //呼び出される。何を表示するのか、どの行を使うのか
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1")! as UITableViewCell
+        //
+        var identifier = ""
+        
+        if indexPath.row % 2 == 0 {
+            identifier = "Cell1"
+        } else {
+            identifier = "Cell2"
+        }
+        //画面作成で名付けたものを返り値に相応しくなるようにキャスト
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)! as UITableViewCell
         return cell
+        
     }
 }
 
