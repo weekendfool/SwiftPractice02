783f64136ddbbaa75bd72f329281feb1d67bd55f
Ohara <nori.tukareta@gmail.com>
2020-06-08 19:28:33 +0900

    遷移前のデータを戻すサンプル処理

---
+++ UnwindSegueDataSampleApp/UnwindSegueDataSampleApp/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  UnwindSegueDataSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/08.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var greenTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ UnwindSegueDataSampleApp/UnwindSegueDataSampleApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UnwindSegueDataSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/08.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var yellowLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //グリーンから戻ってくる処理
+    @IBAction func exitDone(segue:UIStoryboardSegue) {
+        //SecondViewControllerをプロパティに格納する
+        let green  = segue.source as! SecondViewController
+        //yellowLabelに遷移前の画面の値を代入する
+        yellowLabel.text = green.greenTextField.text
+    }
+
+}
+
