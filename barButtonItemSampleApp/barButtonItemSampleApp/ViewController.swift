2ae7985a0052c8047ad291bd3dd38931ae2f618d
Ohara <nori.tukareta@gmail.com>
2020-06-15 01:40:51 +0900

    barButtonItemSample作成

---
+++ barButtonItemSampleApp/barButtonItemSampleApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  barButtonItemSampleApp
+//
+//  Created by 尾原徳泰 on 2020/06/15.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func pressButton(_ sender: Any) {
+        print("test")
+    }
+    
+}
+
