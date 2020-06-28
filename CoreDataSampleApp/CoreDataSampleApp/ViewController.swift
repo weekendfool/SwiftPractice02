7ae9898012a11ac698412dedb3fbb712d16d8aff
Ohara <nori.tukareta@gmail.com>
2020-06-28 10:48:33 +0900

    一旦中断

--- CoreDataSampleApp/CoreDataSampleApp/ViewController.swift
+++ CoreDataSampleApp/CoreDataSampleApp/ViewController.swift
@@ -9,44 +9,66 @@
 import UIKit
 //CoreDataをインポート
 import CoreData
+//import CoreData.NSManagedObjectContext
 
 class ViewController: UIViewController {
     //パーツの紐付け
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
     
-    //
-    var person:[String] = []
-    //
-    var managedContext = (UIApplication.shared.delegate as! AppDelegat).persistentContainer.viewContext
+    var box:[String] = []
+    
+    let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let conditions = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
-        
-        do　{
-            person = try managedContext.fetch(conditions) as! [String]
-        }　catch　{
-            print("エラー")
+    
     }
 
     @IBAction func pressCreateButton(_ sender: Any) {
-        //
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegat {
-            //
-            
-            let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)
+        //entityのアンラップ
+        if let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext) {
+            //テキストのInt化　キャスト
             let age = Int(ageTextField.text!)
+            //インスタンス作成
+            let personObject = NSManagedObject(entity: entity, insertInto: managedContext)
+            //core dataに情報を保存する処理
+            personObject.setValue(nameTextField.text, forKey: "name")
+            personObject.setValue(age, forKey: "age")
             
-        }
-        {
-            let personObject = persistentContainer.
+            
+            
+//            let v = (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+//            //エラー処理
+//            var error:NSError?
+//
+//            if  {
+//                print("保存に成功")
+//            } else {
+//                print("保存に失敗")
+//            }
+        
         }
     }
     
     @IBAction func pressReadButton(_ sender: Any) {
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
+        
+//        var error:NSError?
+        
+        do {
+            box = try managedContext.fetch(fetchRequest) as! [String]
+        } catch {
+            print("error")
+        }
+        
+        for obj in box {
+            print("name: \(obj)")
+        }
     }
 }
 
